ae81b2dba52955ece5468e7101c13bb0e4c9df94
Julia Teleki <juliateleki@tamu.edu>
2022-02-03 20:32:38 -0600

    added unit tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,9 +1,10 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter', author: 'j. k. rolling')
+    described_class.new(title: 'harry potter', author: 'j. k. rolling', price: 10, published_date: DateTime.now())
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +13,18 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+  it 'it is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+  it 'it is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+  it 'it is not valid without a published_date' do
+    subject.published_date = nil
     expect(subject).not_to be_valid
   end
 end
